75302bf33a50ea53dc527f736aa0495fc203ac46
akiyoshi83 <mail@akiyoshi.info>
2016-10-03 07:01:13 +0000

    Add third-party cookbooks

--- cookbooks/dev/metadata.rb
+++ cookbooks/dev/metadata.rb
@@ -6,6 +6,10 @@ description 'Installs/Configures dev'
 long_description 'Installs/Configures dev'
 version '0.1.0'
 
+depends 'vim'
+depends 'git'
+depends 'tmux'
+
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
 # issues_url 'https://github.com/<insert_org_here>/dev/issues' if respond_to?(:issues_url)
