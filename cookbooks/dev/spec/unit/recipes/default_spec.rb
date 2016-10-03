f3b3596e275b5914f7fea1dfbd8a531270d41848
akiyoshi83 <mail@akiyoshi.info>
2016-10-03 07:49:14 +0000

    Add spec

--- cookbooks/dev/spec/unit/recipes/default_spec.rb
+++ cookbooks/dev/spec/unit/recipes/default_spec.rb
@@ -7,14 +7,18 @@
 require 'spec_helper'
 
 describe 'dev::default' do
-  context 'When all attributes are default, on an unspecified platform' do
-    let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
-      runner.converge(described_recipe)
-    end
+  let(:chef_run) do
+    runner = ChefSpec::ServerRunner.new
+    runner.converge(described_recipe)
+  end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+  %w(tree bind-utils vim tmux).each do |name|
+    it "install #{name}" do
+      expect(chef_run).to install_package(name)
     end
+  end
+
+  it 'install git_client[default]' do
+    expect(chef_run).to install_git_client('default')
   end
 end
