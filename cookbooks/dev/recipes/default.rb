96fd16e011ee2f8a9a69f570a10f31084ebf318b
akiyoshi83 <mail@akiyoshi.info>
2016-10-03 07:22:26 +0000

    Add packages to default recipe

--- cookbooks/dev/recipes/default.rb
+++ cookbooks/dev/recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+include_recipe 'vim'
+include_recipe 'git'
+include_recipe 'tmux'
+
+packages = %w(
+  tree
+  bind-utils
+)
+
+packages.each do |name|
+  package name do
+    action :install
+  end
+end
